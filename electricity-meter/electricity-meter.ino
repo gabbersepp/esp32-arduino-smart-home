c46f2cea9ff038283b233701250f5f0853a7a150
gabbersepp <gabbersepp@googlemail.com>
2023-03-03 19:49:27 +0100

    add OTA, Telnet, adjust threshold

--- electricity-meter/electricity-meter.ino
+++ electricity-meter/electricity-meter.ino
@@ -1,9 +1,13 @@
 
 #include <WiFi.h>
 #include <WiFiClient.h>
+#include <ESPmDNS.h>
 #include <iostream>
 #include <PubSubClient.h>
 #include "wlan.h"
+
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 // https://community.home-assistant.io/t/energy-measurement-with-mqtt-sensor/424558/19
 
@@ -14,9 +18,12 @@ const char* mqtt_server = MQTT;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+WiFiServer TelnetServer(23);
+WiFiClient Telnet;
 
 int rounds = 0;
 bool alreadyCount = false;
+long lastPingMillis = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,9 +35,10 @@ void setup() {
   WiFi.begin(ssid, password);
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    delay(5000);
+    Serial.println("restart due to failing Wifi");
+    ESP.restart();
   }
 
   Serial.println("");
@@ -40,6 +48,59 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   client.setServer(mqtt_server, 1883);
+
+  ArduinoOTA.setHostname("electricity-meter");
+  ArduinoOTA.setPassword("admin");
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  TelnetServer.begin();
+  TelnetServer.setNoDelay(true); 
+}
+
+void handleTelnet() {
+  if (TelnetServer.hasClient()) {
+    if (!Telnet || !Telnet.connected()) {
+      if (Telnet) Telnet.stop();
+      Telnet = TelnetServer.available();
+    } else {
+      TelnetServer.available().stop();
+    }
+  }
+}
+
+void sendPing() {
+  if (lastPingMillis + 20000 < millis()) {
+    lastPingMillis = millis();
+    char pingStr[20];
+    dtostrf(lastPingMillis, 1, 2, pingStr);
+    client.publish("home/energy/ping", pingStr);
+  }
 }
 
 void reconnect() {
@@ -63,32 +124,41 @@ int amount = 0;
 long lastMillis = 0;
 
 void loop() {
+  ArduinoOTA.handle();
+  
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+
+  handleTelnet(); 
+
+  sendPing();
 
   unsigned int v = (unsigned int)analogRead(35);
   
   sum += v;
   amount++;
 
-  if (millis() > lastMillis + 1000) {
+  if (millis() > lastMillis + 500) {
     
     Serial.printf("%d\r\n", sum/amount);
+    //Telnet.printf("%d\r\n", sum/amount);
 
-    if (sum/amount > 60 && !alreadyCount) {
+    if (sum/amount > 24 && !alreadyCount) {
+        Telnet.printf("avg: %d, millis: %d\r\n", sum/amount, (int)millis());
           rounds++;
           alreadyCount = true;
           Serial.printf("%u\r\n", v);
           Serial.printf("New round: %d\r\n", rounds);
+          //Telnet.printf("New round: %d\r\n", rounds);
           
           // send by mqtt
           char tempString[8];
           dtostrf(rounds, 1, 2, tempString);
           client.publish("home/energy/rounds", tempString);
           client.publish("home/energy/round", "1");
-      } else if (sum/amount <= 60) {
+      } else if (sum/amount <= 24) {
         alreadyCount = false;
       }
     
